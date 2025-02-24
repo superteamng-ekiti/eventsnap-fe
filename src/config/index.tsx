c18f9de11f543bd7000b28d9de7fba35d6ca197e
inspi-writer <94226358+inspi-writer001@users.noreply.github.com>
2025-02-24 18:23:57 +0100

    fix:added tesnet and mainne rpc details

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,21 +1,89 @@
 import { PROJECT_ID } from "@/constant";
-import { BaseWalletAdapter, SolanaAdapter } from "@reown/appkit-adapter-solana/react";
-import { solanaDevnet, solana, solanaTestnet } from "@reown/appkit/networks";
+import {
+  BaseWalletAdapter,
+  SolanaAdapter
+} from "@reown/appkit-adapter-solana/react";
+import {
+  solanaDevnet,
+  solana,
+  solanaTestnet,
+  defineChain
+} from "@reown/appkit/networks";
 import { createAppKit } from "@reown/appkit/react";
 import {
   PhantomWalletAdapter,
-  SolflareWalletAdapter,
+  SolflareWalletAdapter
 } from "@solana/wallet-adapter-wallets";
 
-// import { defineChain } from '@reown/appkit/networks';
+const sonicSvmTestnet = defineChain({
+  blockExplorers: {
+    default: {
+      name: "Sonic Explorer",
+      url: "https://explorer.sonic.game"
+    }
+  },
+  name: "Sonic SVM Testnet",
 
-// const devnetSonic = defineChain({
-//   id: "sonic-devnet",
-// })
+  // Keep the Solana currency details if the native currency is still SOL
+  nativeCurrency: {
+    name: "Solana",
+    symbol: "SOL",
+    decimals: 9
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://api.testnet.sonic.game"]
+    }
+  },
 
+  testnet: true,
+  chainNamespace: "solana",
+
+  // to be updatted
+  id: "4uhcVJyU9pJkvQyS88uRDiswHXSCkY3z",
+  caipNetworkId: "solana:4uhcVJyU9pJkvQyS88uRDiswHXSCkY3z",
+
+  custom: {
+    faucetUrl: "https://faucet.sonic.game",
+    mainnetAlphaUrl: "https://api.mainnet-alpha.sonic.game"
+  },
+
+  network: "sonic-testnet"
+});
+export const sonicSvmMainnet = defineChain({
+  blockExplorers: {
+    default: {
+      name: "Sonic Explorer",
+      url: "https://explorer.sonic.game"
+    }
+  },
+
+  name: "Sonic SVM Mainnet",
+  nativeCurrency: {
+    name: "Solana",
+    symbol: "SOL",
+    decimals: 9
+  },
+
+  rpcUrls: {
+    default: {
+      http: ["https://api.mainnet-alpha.sonic.game"]
+    }
+  },
+
+  testnet: false,
+  chainNamespace: "solana",
+  id: "5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp",
+
+  caipNetworkId: "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp",
+  network: "sonic-mainnet"
+});
 
 export const solanaWeb3JsAdapter = new SolanaAdapter({
-  wallets: [new PhantomWalletAdapter() as unknown as BaseWalletAdapter<string>, new SolflareWalletAdapter() as unknown as BaseWalletAdapter<string>],
+  wallets: [
+    new PhantomWalletAdapter() as unknown as BaseWalletAdapter<string>,
+    new SolflareWalletAdapter() as unknown as BaseWalletAdapter<string>
+  ]
 });
 
 export const projectId = PROJECT_ID ?? "";
@@ -24,15 +92,16 @@ export const metadata = {
   name: "AppKit",
   description: "AppKit Solana Example",
   url: "https://example.com", // origin must match your domain & subdomain
-  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+  icons: ["https://avatars.githubusercontent.com/u/179229932"]
 };
 
-export const createAppKitMethod = () => createAppKit({
-  adapters: [solanaWeb3JsAdapter],
-  networks: [solana, solanaTestnet, solanaDevnet],
-  metadata: metadata,
-  projectId: projectId,
-  features: {
-    analytics: true,
-  },
-});
+export const createAppKitMethod = () =>
+  createAppKit({
+    adapters: [solanaWeb3JsAdapter],
+    networks: [sonicSvmTestnet, sonicSvmMainnet],
+    metadata: metadata,
+    projectId: projectId,
+    features: {
+      analytics: true
+    }
+  });
