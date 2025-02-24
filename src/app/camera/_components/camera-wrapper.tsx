7bcdfb6a2b82fe6df29111419d1fc6bef5d0e951
EmmanuelOluwafemi <emaz4me@gmail.com>
2025-02-24 12:57:39 +0100

    feat: implement camera sdk component and cleanup

--- src/app/camera/_components/camera-wrapper.tsx
+++ src/app/camera/_components/camera-wrapper.tsx
@@ -2,13 +2,31 @@
 
 import React, { useState } from 'react'
 import { CameraShotter } from './camera-shotter';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export const CameraWrapper = () => {
     const [capturedImage, setCapturedImage] = useState<string | null>(null);
 
     return (
         <div className='w-full flex flex-col items-center justify-center'>
-            <CameraShotter type="portrait" setCapturedImage={setCapturedImage} />
+            {
+                capturedImage ? (
+                    <div className='w-full h-full min-h-[100svh] relative'>
+                        <Image src={capturedImage} alt="captured" fill className='object-cover' />
+                        <div className='absolute bottom-8 left-1/2 -translate-x-1/2 z-10 flex items-center justify-between w-full px-4'>
+                            <Button variant="outline" onClick={() => setCapturedImage(null)}>
+                                Retake Photo
+                            </Button>
+                            <Button>
+                                Upload Photo
+                            </Button>
+                        </div>
+                    </div>
+                ) : (
+                    <CameraShotter type="portrait" setCapturedImage={setCapturedImage} />
+                )
+            }
         </div>
     )
 }
--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -5,6 +5,7 @@ import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { recoletaRegular } from "@/app/font"
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -17,7 +18,7 @@ const Label = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={cn(labelVariants(), `${recoletaRegular.className}`, className)}
     {...props}
   />
 ))
